0ecaf12ff1df0b00b7289148be61a1e345f2fc28
ed-sa-ma <28660051+ed-sa-ma@users.noreply.github.com>
2022-08-27 18:39:04 +0200

    Add create route

--- src/emotions-manage/emotions-m.controller.js
+++ src/emotions-manage/emotions-m.controller.js
@@ -6,3 +6,13 @@ export const getEmotionsManageInfo = async (req, res) => {
   const emotions = await col.find().toArray(); // busco TODOS y lo paso a array
   res.json(emotions); // devuelvo el resultado al cliente
 };
+
+// CREATE ONE
+export const createEmotionManage = (req, res) => {
+  const { title, emotionsManage, image } = req.body // Get the emotion data from the request body
+
+  const db = req.app.locals.ddbbClient.db("final-project"); // cojo la BBDD
+  const col = db.collection("emotions-manage") // cojo la collection
+  col.insertOne({ title, emotionsManage, image });
+  res.sendStatus(200);
+}
--- src/emotions-manage/emotions-m.router.js
+++ src/emotions-manage/emotions-m.router.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getEmotionsManageInfo } from "./emotions-m.controller.js";
+import { createEmotionManage, getEmotionsManageInfo } from "./emotions-m.controller.js";
 
 const router = express.Router();
 
 router.route("/").get(getEmotionsManageInfo);
+
+router.route("/create").post(createEmotionManage);
 
 export default router;
