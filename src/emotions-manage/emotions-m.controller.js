816ddc03f81bab89050dca37cff299adcd7c25e2
Vict Mart <codigovictor01@gmail.com>
2022-09-14 18:53:30 +0200

    session users cards

--- src/emotions-manage/emotions-m.controller.js
+++ src/emotions-manage/emotions-m.controller.js
@@ -1,19 +1,25 @@
 import { ObjectId } from "mongodb";
+import { getEmailToken } from "../auth/auth.middleware.js";
 
 // GET ALL
 export const getEmotionsManageInfo = async (req, res) => {
   // ir a BBDD y devolver TODOS los users
-
+  const email = getEmailToken(req, res);
   const db = req.app.locals.ddbbClient.db("final-project"); // cojo la BBDD
   const col = db.collection("emotions-manage"); // cojo la collection
-  const emotions = await col.find().toArray(); // busco TODOS y lo paso a array
+  const emotions = await col.find({ userCreator: email }).toArray(); // busco TODOS y lo paso a array
   res.json(emotions); // devuelvo el resultado al cliente
 };
 
 export const createEmotionManage = async (req, res) => {
   const db = req.app.locals.ddbbClient.db("final-project"); // cojo la BBDD
   const col = db.collection("emotions-manage"); // cojo la collection
+  const email = getEmailToken(req, res);
+  console.log(" mi email es ", email);
   const emotion = req.body; // cojo el body del cliente
+  emotion.userCreator = email;
+  if (emotion.url == "") delete emotion.url;
+  console.log(emotion);
   const result = await col.insertOne(emotion); // inserto el user en la collection
   res.json(result); // devuelvo el resultado al cliente
 };
@@ -41,7 +47,9 @@ export const editEmotionManage = async (req, res) => {
   const db = req.app.locals.ddbbClient.db("final-project"); // cojo la BBDD
   const col = db.collection("emotions-manage"); // cojo la collection
   const id = req.params._id;
+  const email = getEmailToken(req, res);
   const emotion = req.body; // cojo el body del cliente
+  emotion.userCreator = email;
   const result = await col.findOneAndUpdate(
     { _id: ObjectId(id) },
     { $set: emotion },
